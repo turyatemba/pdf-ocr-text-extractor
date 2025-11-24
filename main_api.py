1d3d55df1cc0dfc0b46e56648dcc090ac9b67ff6
ronald turyatemba <rotemba81@gmail.com>
2025-11-24 14:20:10 +0300

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -15,7 +15,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://front-end-pdf-extractor.vercel.app"],  # or your domain
-    allow_credentials=True,
+    #allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
